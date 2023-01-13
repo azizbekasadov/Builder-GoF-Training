24b44258632418706013ca53cd4e96b9b4b86901
Azizbek Asadov <azizbek1998@icloud.com>
2023-01-13 20:46:16 +0500

    Initial Commit

---
+++ 5. Builder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5. Builder
+//
+//  Created by Azizbek Asadov on 13/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
